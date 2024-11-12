2a75d4799dd5221b0a0e85ef7bec1ac067292a8e
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2024-11-12 20:57:15 -0300

    :white_check_mark: add campaign tests

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,45 +1,44 @@
 package campaign
 
 import (
+	"strings"
 	"time"
 
-	"github.com/google/uuid"
+	"github.com/rs/xid"
 )
 
-
 type Contact struct {
-	ID        string `json:"id"`
-	Name			string `json:"name"`
-	Email			string `json:"email"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 type Campaign struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	Content   string `json:"content"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Content   string    `json:"content"`
 	Contacts  []Contact `json:"contacts"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
-
-
 func NewCampaign(name string, content string, emails []string) *Campaign {
-
-	contacts := make([]Contact, len(emails))
+	guid := xid.New()
+	contacts := make([]Contact, 0, len(emails))
 
 	for _, email := range emails {
 		contact := Contact{
-			ID:        uuid.New().String(),
-			Name:      "Name",
+			ID:        guid.String(),
+			Name:      strings.Split(email, "@")[0],
 			Email:     email,
 			CreatedAt: time.Now(),
 		}
+
 		contacts = append(contacts, contact)
 	}
 
 	return &Campaign{
-		ID:        uuid.New().String(),
+		ID:        guid.String(),
 		Name:      name,
 		Content:   content,
 		Contacts:  contacts,
--- internal/domain/campaign/campaing_test.go
+++ internal/domain/campaign/campaing_test.go
@@ -1,1 +1,45 @@
 package campaign
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+var (
+	name = "Test Campaign"
+	content = "<h1>Test Content</h1>"
+	contacts = []string{"contact01@email.com", "contact02@email.com"}
+)
+
+func Test_NewCampaign_CreateCampaign(t *testing.T){
+	assert := assert.New(t)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.Equal(name, campaign.Name)
+	assert.Equal(content, campaign.Content)
+	assert.Len(campaign.Contacts, 2)
+} 
+
+func Test_NewCampaign_IdIsNotNil(t *testing.T){
+	assert := assert.New(t)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+}
+
+
+func Test_NewCampaign_CreateAtMustBeNow(t *testing.T){
+	assert := assert.New(t)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	dateNow := time.Now().Add(-time.Minute)
+
+	assert.NotNil(campaign.CreatedAt)
+	assert.Greater(campaign.CreatedAt, dateNow)
+}
