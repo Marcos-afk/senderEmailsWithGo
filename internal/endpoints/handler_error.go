6ed50dea93ba856007b77917c7f5339ed4e2d9fb
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2024-12-03 01:34:04 -0300

    :sparkles: add error validation for 404 status

--- internal/endpoints/handler_error.go
+++ internal/endpoints/handler_error.go
@@ -4,42 +4,47 @@ import (
 	"errors"
 	"net/http"
 	internalerrors "senderEmails/internal/internal-errors"
+	"strings"
 
 	"github.com/go-chi/render"
 )
 
-type EndpointFunc func(w http.ResponseWriter, r *http.Request)(interface{}, int, error)
+type EndpointFunc func(w http.ResponseWriter, r *http.Request) (interface{}, int, error)
 
-func HandlerError(endpointFunc EndpointFunc) http.HandlerFunc{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-      obj, status, err := endpointFunc(w, r)
+func HandlerError(endpointFunc EndpointFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		obj, status, err := endpointFunc(w, r)
 
-			if err != nil {
-				if errors.Is(err, internalerrors.ErrInternal) {
-					render.Status(r, http.StatusInternalServerError)
-				} else {
-					render.Status(r, http.StatusBadRequest)
-				}
+		var statusCode int
 
-				errorResponse := struct {
-					Message string `json:"message"`
-					Status int32 `json:"status"`
-				}{
-					Message: err.Error(),
-					Status: 400,
-				}
-		
-				render.JSON(w, r, errorResponse)
-				return
+		if err != nil {
+			
+			if errors.Is(err, internalerrors.ErrInternal){
+				statusCode = http.StatusInternalServerError
+			} else if strings.Contains(err.Error(), "não encontrada") || strings.Contains(err.Error(), "não encontrado"){
+				statusCode = http.StatusNotFound
+			}else{
+				statusCode = http.StatusBadRequest
 			}
 
-			render.Status(r, status)
-			if obj != nil {
-				render.JSON(w, r, obj)
-				return
-			}
-      
-			render.Status(r, http.StatusNoContent)
-			render.NoContent(w, r)
+			render.Status(r, statusCode)
+			render.JSON(w, r, struct {
+				Message string `json:"message"`
+				Status  int32  `json:"status"`
+			}{
+				Message: err.Error(),
+				Status:  int32(statusCode),
+			})
+			return
+		}
+
+		render.Status(r, status)
+
+		if obj != nil {
+			render.JSON(w, r, obj)
+			return
+		}
+
+		render.NoContent(w, r)
 	})
 }
